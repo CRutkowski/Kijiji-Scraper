200d88ef27ba967136e28b534db3853e62b038f6
André Cooke <Andr Cooke>
2017-03-11 08:06:54 -0500

    Completed scripts

--- Kijiji-Scraper.py
+++ Kijiji-Scraper.py
@@ -1,8 +1,6 @@
 #!C:\Python34\scrapper\Scripts
 
-
 # Place url, linking to ad list, with desired search filters here.
-# e.g. http://www.kijiji.ca/b-cars-trucks/calgary/convertible__coupe__hatchback__other+body+type__sedan__wagon-mazda-mx5miata/c174l1700199a138a54a1000054
 url_to_scrape = "http://www.kijiji.ca/b-canot-kayak-paddle-board/quebec/kayak/k0c329l9001"
 
 # Set the delay in (s) that the programs waits before scraping again.
@@ -13,6 +11,7 @@ filename = 'ads.txt'
 
 import requests
 from bs4 import BeautifulSoup
+
 import datetime
 import time
 
@@ -20,41 +19,35 @@ import time
 def ParseAd(html):  # Parses ad html trees and sorts relevant data into a dictionary
     ad_info = {}
     try:
-        ad_info["Url"] = 'http://www.kijiji.ca' + html.get("data-vip-url")
-    except:
-        log('[Error] Unable to parse URL data.')
-
-    try:
         ad_info["Title"] = html.find_all('a', {"class": "title"})[0].text.strip()
     except:
         log('[Error] Unable to parse Title data.')
 
     try:
-        description, details = html.find_all('p')
-        ad_info["Description"] = description = description.text.strip()
+        ad_info["Url"] = 'http://www.kijiji.ca' + html.get("data-vip-url")
+    except:
+        log('[Error] Unable to parse URL data.')
+
+    try:
+        ad_info["Description"] = html.find_all('div', {"class": "description"})[0].text.strip()
     except:
         log('[Error] Unable to parse Description data.')
 
     try:
-        checklist = ['Boréal', 'Kayak de mer']
-        details = details.text.split('|')
-        for item in details:
-            if item == '\n':
-                pass
-            if 'km' in item:
-                ad_info["Mileage"] = item.strip()
-            if item.strip() in checklist:
-                ad_info["Trans"] = item.strip()
+        tempsoup = html.find_all('div', {"class": "location"})[0].text.strip()
+        if tempsoup.find('-') > 0:
+            tempsoup = tempsoup[:tempsoup.find('-') - 2]
+        ad_info["Location"] = tempsoup
     except:
-        log('[Error] Unable to parse Mileage/Trans data.')
+        log('[Error] Unable to parse Location data.')
 
     try:
-        ad_info["Date"] = html.find_all('span ', {"class": "date-posted"})[0].text.strip()
+        ad_info["Date"] = html.find_all('span', {"class": "date-posted"})[0].text.strip()
     except:
         log('[Error] Unable to parse Date data.')
 
     try:
-        ad_info["Price"] = html.find_all('div ', {"class": "price"})[0].text.strip()
+        ad_info["Price"] = html.find_all('div', {"class": "price"})[0].text.strip()
     except:
         log('[Error] Unable to parse Price data.')
 
@@ -95,21 +88,21 @@ def ReadAds(filename):  # Reads given file and creates a dict of ads in file
 
 def log(text):  # writes log data to log.txt with datetime.
     date_time = datetime.datetime.now()
-    file = open('log.txt', 'a')
+    myfile = open('log.txt', 'a')
     date_time = str(date_time) + '\n'
-    text = text + '\n\n'
-    file.write(date_time)
-    file.write(text)
-    file.close()
+    text += '\n\n'
+    myfile.write(date_time)
+    myfile.write(text)
+    myfile.close()
 
 
 def MailAd(ad_dict):  # Sends an email with a link and info of new ads
     import smtplib
     from email.mime.text import MIMEText
 
-    sender = 'andrecooke@hotmail.com'
-    passwd = '********'
-    receiver = 'andrecooke@hotmail.com'
+    sender = 'email@example.com'
+    passwd = 'Password'
+    receiver = 'email@example.com'
 
     count = len(ad_dict)
     if count > 1:
@@ -120,18 +113,14 @@ def MailAd(ad_dict):  # Sends an email with a link and info of new ads
     body = ''
     try:
         for ad_id in ad_dict:
-            body = body + ad_dict[ad_id]['Title'] + '\n'
-            body = body + ad_dict[ad_id]['Url'] + '\n\n'
-            body = body + ad_dict[ad_id]['Description'] + '\n\n'
-            if 'Mileage' in ad_dict[ad_id]:
-                body = body + ad_dict[ad_id]['Mileage'] + '\t\t' + ad_dict[ad_id]['Trans'] + '\n'
-            if 'Prix' in ad_dict[ad_id]:
-                body = body + ad_dict[ad_id]['Prix']
-            body = body + '\n\n\n'
-        body = body + 'This is an automated message.\nPlease do not reply to this message.'
+            body += ad_dict[ad_id]['Title'] + ' - ' + ad_dict[ad_id]['Price'] + ' - ' + ad_dict[ad_id]['Location']
+            body += ' - ' + ad_dict[ad_id]['Date'] + '\n'
+            body += ad_dict[ad_id]['Url'] + '\n\n'
+
     except:
         log('[Error] Unable to create body for email message')
 
+    body += 'This is an automated message.\nPlease do not reply to this message.'
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
@@ -162,27 +151,23 @@ def main(old_ad_dict):  # Main function, brings it all together.
         log("[Okay] Retrieved HTML data from: " + url_to_scrape)
     except:
         log("[Error] Unable to load html data from: " + url_to_scrape)
+
     soup = BeautifulSoup(page.content, "html.parser")
-#    print(soup.title)
-#    for link in soup.find_all('div'):
-#        if link.get('data-ad-id') is not None:
-#            print(link.get('data-ad-id'))
-#            print(link.get('data-vip-url'))
-
-
     page = None
 
     kijiji_ads = soup.find_all("div", {"class": "regular-ad"})  # Finds all ad trees in page html.
 
     ad_dict = {}
-
+    checklist = ['boréal', 'kayak de mer', 'baffin', 'epsilon', 'scorpio']
+    excludelist = ['wanted', 'recherché']
     for ad in kijiji_ads:  # Creats a dictionary of all ads sorted by ad id.
-        title = ad.find_all('a', {"class": "title"})[0].text.strip()[0:6]
+        title = ad.find_all('a', {"class": "title"})[0].text.strip()
         ad_id = ad.find_all('div', {'class': "watch"})[0].get('data-adid')
-        if not title == "Wanted":  # Checks and skips any ad that's "Wanted".
-            if ad_id not in old_ad_dict:
-                log('[Okay] New ad found! Ad id: ' + ad_id)
-                ad_dict[ad_id] = ParseAd(ad)
+        if not [False for match in excludelist if match in title.lower()]:
+            if [True for match in checklist if match in title.lower()]:
+                if ad_id not in old_ad_dict:
+                    log('[Okay] New ad found! Ad id: ' + ad_id)
+                    ad_dict[ad_id] = ParseAd(ad)
 
     if ad_dict != {}:  # If dict not emtpy, write ads to text file and send email.
         WriteAds(ad_dict, filename)
@@ -199,6 +184,6 @@ def main(old_ad_dict):  # Main function, brings it all together.
 if __name__ == "__main__":
     old_ad_dict = ReadAds(filename)
     log("[Okay] Ad database succesfully loaded.")
-    file = open('log.txt', 'w')  # Create/Empty log file
-    file.close()
+    myfile = open('log.txt', 'w')  # Create/Empty log file
+    myfile.close()
     main(old_ad_dict)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Kijiji-Scraper
+# Kijiji-Scraper 2.0
 Python script that scrapes Kijiji ad information and sends out an email
 when a new ad is found.
 It's designed to be run in the background (Pythonw).
@@ -8,9 +8,15 @@ Dependencies: requests and BeautifulSoup
 To configure the script:
 -Set the 'url_to_scrape' variable to the url of the Kijiji ad page you want to scrape.
     e.g. 'http://www.kijiji.ca/b-cars-trucks/calgary/convertible__coupe__hatchback__other+body+type__sedan__wagon-mazda-mx5miata/c174l1700199a138a54a1000054'
-    
--Set the 'scrape_delay' variable to them time in seconds before re-scraping. (default = 600)
 
--Replace SENDER_EMAIL, SENDER_EMAIL_PASSWORD, RECEIVER_EMAIL fields in the 'MailAd' function with your email info.
+- Set the checklist with word required for ad to be selected / included
 
--If you're using gmail, you'll have to go to 'My Account>Sign in & security>Connected apps & sites' then turn "Allow less secure apps" to "On".
+- Set the excludelist with word for ads to be excluded.
+
+- Set the 'scrape_delay' variable to them time in seconds before re-scraping. (default = 600)
+
+- Replace SENDER_EMAIL, SENDER_EMAIL_PASSWORD, RECEIVER_EMAIL fields in the 'MailAd' function with your email info.
+
+- If you're using gmail, you'll have to go to 'My Account>Sign in & security>Connected apps & sites' then turn "Allow less secure apps" to "On".
+
+
