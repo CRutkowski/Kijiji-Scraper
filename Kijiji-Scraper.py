0f5dd1479ab9d9e28f0ef48562d34075ae1778eb
Chase Rutkowski <33402674+crutkows@users.noreply.github.com>
2018-04-29 23:09:23 -0600

    Added parsing of multiple pages
    The scraper will now search through all pages of ads instead of just the first page

--- Kijiji-Scraper.py
+++ Kijiji-Scraper.py
@@ -62,13 +62,12 @@ def ParseAd(html):  # Parses ad html trees and sorts relevant data into a dictio
     return ad_info
 
 
-def WriteAds(ad_dict, filename):  # Writes ads to given file
+def WriteAds(ad_dict, filename):  # Writes ads from given dictionary to given file
     try:
         file = open(filename, 'a')
         for ad_id in ad_dict:
             file.write(ad_id)
             file.write(str(ad_dict[ad_id]) + "\n")
-            print('[Okay] Ad ' + ad_id + ' written to file.')
         file.close()
     except:
         print('[Error] Unable to write ad(s) to file.')
@@ -81,15 +80,14 @@ def ReadAds(filename):  # Reads given file and creates a dict of ads in file
         file.close()
 
     ad_dict = {}
-    file = open(filename, 'r')
-    for line in file:
-        if line.strip() != '':
-            index = line.find('{')
-            ad_id = line[:index]
-            dictionary = line[index:]
-            dictionary = ast.literal_eval(dictionary)
-            ad_dict[ad_id] = dictionary
-    file.close()
+    with open(filename, 'r') as file:
+        for line in file:
+            if line.strip() != '':
+                index = line.find('{')
+                ad_id = line[:index]
+                dictionary = line[index:]
+                dictionary = ast.literal_eval(dictionary)
+                ad_dict[ad_id] = dictionary
     return ad_dict
 
 
@@ -97,9 +95,15 @@ def MailAd(ad_dict, email_title):  # Sends an email with a link and info of new 
     import smtplib
     from email.mime.text import MIMEText
 
+    
+    # Fill in the variables below with your info
+    #------------------------------------------
     sender = 'sender@example.com'
     passwd = 'Sender Password'
     receiver = 'receiver@example.com'
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 465
+    #------------------------------------------
 
     count = len(ad_dict)
     if count > 1:
@@ -115,20 +119,23 @@ def MailAd(ad_dict, email_title):  # Sends an email with a link and info of new 
             body += '<a href="' + ad_dict[ad_id]['Url'] + '">'
             body += ad_dict[ad_id]['Image'] + '</a>'
             body += '<p>' + ad_dict[ad_id]['Description'] + '<br />'
-            body += ad_dict[ad_id]['Details'] + '<br />' + ad_dict[ad_id]['Price'] + '<br /><br /><br /><br /></p>'
+            if ad_dict[ad_id]['Details'] != '':
+                body += ad_dict[ad_id]['Details'] + '<br />' + ad_dict[ad_id]['Price'] + '<br /><br /><br /><br /></p>'
+            else:
+                body += ad_dict[ad_id]['Price'] + '<br /><br /><br /><br /></p>'
     except:
         body +='<p>' +  ad_dict[ad_id]['Title'] + '<br />'
         body += ad_dict[ad_id]['Url'] + '<br /><br />' + '</p>'
         print('[Error] Unable to create body for email message')
 
-    body += '<p>This is an automated message.\nPlease do not reply to this message.</p>'
+    body += '<p>This is an automated message, please do not reply to this message.</p>'
     msg = MIMEText(body, 'html')
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = receiver
 
     try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
         server.ehlo()
     except:
         print('[Error] Unable to connect to email server.')
@@ -144,42 +151,58 @@ def MailAd(ad_dict, email_title):  # Sends an email with a link and info of new 
         print('[Error] Unable to send message.')
 
 
-def scrape(url, old_ad_dict, exclude_list, filename):  # Main function, brings it all together.
-    try:
-        page = requests.get(url)
-    except:
-        print("[Error] Unable to load html data from: " + url)
-        sys.exit(1)
-
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    email_title = soup.find('div', {'class': 'message'}).find('strong').text.strip('"')
-    email_title = toUpper(email_title)
-    kijiji_ads = soup.find_all("div", {"class": "regular-ad"})  # Finds all ad trees in page html.
-
+def scrape(url, old_ad_dict, exclude_list, filename):  # Pulls page data from a given kijiji url and finds all ads on each page
+    # Initialize variables for loop
+    email_title = None
     ad_dict = {}
-    exclude_list = toLower(exclude_list)
-    #checklist = ['miata']
-    for ad in kijiji_ads:  # Creates a dictionary of all ads sorted by ad id.
-        title = ad.find_all('a', {"class": "title"})[0].text.strip()
-        ad_id = ad.find_all('div', {'class': "watch"})[0].get('data-adid')
-        if not [False for match in exclude_list if match in title.lower()]:
-            #if [True for match in checklist if match in title.lower()]:
-            if ad_id not in old_ad_dict:
-                print('[Okay] New ad found! Ad id: ' + ad_id)
-                ad_dict[ad_id] = ParseAd(ad)
+    third_party_ad_ids = []
+    
+    while url: 
+    
+        try:
+            page = requests.get(url) # Get the html data from the URL
+        except:
+            print("[Error] Unable to load " + url)
+            sys.exit(1)
+    
+        soup = BeautifulSoup(page.content, "html.parser")
+        
+        if not email_title: # If the email title doesnt exist pull it form the html data
+            email_title = soup.find('div', {'class': 'message'}).find('strong').text.strip('"')
+            email_title = toUpper(email_title)
+            
+        kijiji_ads = soup.find_all("div", {"class": "regular-ad"})  # Finds all ad trees in page html.
+        
+        third_party_ads = soup.find_all("div", {"class": "third-party"}) # Find all third-party ads to skip them
+        for ad in third_party_ads:
+            third_party_ad_ids.append(ad['data-ad-id'])
+            
+    
+        exclude_list = toLower(exclude_list) # Make all words in the exclude list lower-case
+        #checklist = ['miata']
+        for ad in kijiji_ads:  # Creates a dictionary of all ads with ad id being the keys.
+            title = ad.find('a', {"class": "title"}).text.strip() # Get the ad title
+            ad_id = ad['data-ad-id'] # Get the ad id
+            if not [False for match in exclude_list if match in title.lower()]: # If any of the title words match the exclude list then skip
+                #if [True for match in checklist if match in title.lower()]:
+                if (ad_id not in old_ad_dict and ad_id not in third_party_ad_ids): # Skip third-party ads and ads already found
+                    print('[Okay] New ad found! Ad id: ' + ad_id)
+                    ad_dict[ad_id] = ParseAd(ad) # Parse data from ad
+        url = soup.find('a', {'title' : 'Next'})
+        if url:
+            url = 'https://www.kijiji.ca' + url['href']
 
     if ad_dict != {}:  # If dict not emtpy, write ads to text file and send email.
-        WriteAds(ad_dict, filename)
-        MailAd(ad_dict, email_title)
+        WriteAds(ad_dict, filename) # Save ads to file
+        MailAd(ad_dict, email_title) # Send out email with new ads
             
-def toLower(input_list):
+def toLower(input_list): # Rturns a given list of words to lower-case words
     output_list = list()
     for word in input_list:
         output_list.append(word.lower())
     return output_list
 
-def toUpper(title):
+def toUpper(title): # Makes the first letter of every word upper-case
     new_title = list()
     title = title.split()
     for word in title:
@@ -190,7 +213,7 @@ def toUpper(title):
         new_title.append(new_word)
     return ' '.join(new_title)
 
-def main():
+def main(): # Main function, handles command line arguments and calls other functions for parsing ads
     args = sys.argv
     if args[1] == '-h' or args[1] == '--help': # Print script usage help
         print('Usage: Kijiji-Scraper.py URL [-f] [-e]\n')
